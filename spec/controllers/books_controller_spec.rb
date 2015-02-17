1ad332554e28868cc357ddac8e701f5fd4c5a026
Brandon Kearns <brandon.j.kearns@gmail.com>
2015-02-17 10:22:07 -0500

    Start book model spec

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
+  let(:book1) { Book.create(title: "Brave New World", author: "Aldous Huxley", thumbnail: "brave.jpg", page_count: 500, pages_read: 250) }
+  let(:book2) { Book.create(title: "Great Gatsby", author: "F Scott Fitzgerald", thumbnail: "old_sport.jpg", page_count: 450, pages_read: 225) }
 
+  describe  
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:book)  { Book.new(title: "Native Son", author: "Richard Wright", thumbnail: "native_son.jpg", page_count: 450, pages_read: 200) }
+
+  subject { book }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:author) }
+  it { should respond_to(:thumbnail) }
+  it { should respond_to(:page_count) }
+  it { should respond_to(:pages_read) }
+
+  it { should be_valid }
+
+  describe 'validations' do
+    describe 'title' do
+      context 'not present' do
+      end
+    end
+
+    describe 'author' do
+      context 'not present' do
+      end
+    end
+
+    describe 'thumbnail' do
+      context 'not present' do
+      end
+    end
+
+    describe 'page count' do
+      context 'not present' do
+      end
+    end
+
+    describe 'pages read' do
+      context 'not present' do
+      end
+    end
+
+  end
 end
